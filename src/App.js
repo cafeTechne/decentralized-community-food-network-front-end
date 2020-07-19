b607ea9596ad9436458f6e6673b8b8946c4223a7
Gino <cafetechne@gmail.com>
2020-07-18 22:21:27 -0400

    Added react-router and then put together a responsive Navbar using bootstrap-react components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,154 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Button, Nav, Navbar, Form, FormControl, Container, Col, Row, NavDropdown, ButtonGroup } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <Router>
+
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+
+        <Navbar.Brand href="/"><h2>D.C.N.F.</h2></Navbar.Brand>
+
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+
+            <Nav.Item>
+              <LinkContainer to="/signUp">
+                <Nav.Link to="/signUp">
+
+                  <ButtonGroup className="d-flex">
+                    <Button variant="danger">
+                      Sign Up
+                </Button>
+                  </ButtonGroup>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+
+
+            <Nav.Item>
+              <LinkContainer to="/contact">
+                <Nav.Link to="/contact">
+                  <ButtonGroup className="d-flex">
+                    <Button variant="info">
+                      Contact
+                </Button>
+                  </ButtonGroup>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+
+
+            <Nav.Item>
+              <LinkContainer to="/donate">
+                <Nav.Link to="/donate">
+                  <ButtonGroup className="d-flex">
+                    <Button variant="success">
+                      Donate
+                </Button>
+                  </ButtonGroup>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+
+
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
+
+
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+
+          <Route path="/signUp">
+            <SignUp />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+
+          <Route path="/donate">
+            <Donate />
+          </Route>
+
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <div>
+    <Container>
+      <Row>
+        <Col><h2>Home</h2></Col>
+      </Row>
+    </Container>
+  </div>;
+}
+
+function SignUp() {
+  return <div>
+    <Container>
+      <Row>
+        <Col><h2>SignUp</h2></Col>
+      </Row>
+    </Container>
+  </div>;
+
+}
+
+function Donate() {
+  return <div>
+    <Container>
+      <Row>
+        <Col><h2>Donate</h2></Col>
+      </Row>
+    </Container>
+  </div>;
+}
+
+function Contact() {
+  return <div>
+    <Container>
+      <Row>
+        <Col><h2>Contact</h2></Col>
+      </Row>
+    </Container>
+  </div>;
+}
+
+function HasLand() {
+  return <h2>I have land!</h2>;
+}
+
+function HasHands() {
+  return <h2>I have hands!</h2>;
+}
